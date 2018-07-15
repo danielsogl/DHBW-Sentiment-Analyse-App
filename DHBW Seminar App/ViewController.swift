fe8bfb652fcd4e90badc34e7000c528191c599e5
Daniel <mytechde@outlook.com>
2018-07-15 10:26:31 +0200

    Initial Commit

---
+++ DHBW Seminar App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DHBW Seminar App
+//
+//  Created by Daniel Sogl on 15.07.18.
+//  Copyright © 2018 Daniel Sogl. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
